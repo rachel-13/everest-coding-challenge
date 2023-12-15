39b302f92b753fe584a8a2b44c8471fd04471f06
Rachel Fong <rach_pohyee13@hotmail.com>
2023-12-15 16:57:16 +0800

    Setup project and test target

---
+++ EverestPackageDeliveryCLITests/EverestPackageDeliveryCLITests.swift
@@ -0,0 +1,20 @@
+//
+//  EverestPackageDeliveryCLITests.swift
+//  EverestPackageDeliveryCLITests
+//
+//  Created by pohz on 15/12/2023.
+//
+
+import XCTest
+
+final class EverestPackageDeliveryCLITests: XCTestCase {
+  
+  override func setUp() {
+    
+  }
+  
+  override func tearDown() {
+    
+  }
+  
+}
