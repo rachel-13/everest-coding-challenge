8640afeb576dfb044605c37a77a768cc6ae64894
Rachel Fong <rach_pohyee13@hotmail.com>
2023-12-19 17:47:32 +0800

    Setup for Problem 2

--- EverestPackageDeliveryCLI/Problem_1/PackageDelivery.swift
+++ EverestPackageDeliveryCLI/Problem_1/PackageDelivery.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-@main
+//@main
 class PackageDelivery {
   
   var isMetadataSet = false
---
+++ EverestPackageDeliveryCLI/RouteOptimizer.swift
@@ -0,0 +1,28 @@
+//
+//  RouteOptimizer.swift
+//  EverestPackageDeliveryCLI
+//
+//  Created by pohz on 19/12/2023.
+//
+
+import Foundation
+
+@main
+class RouteOptimizer {
+  
+  static func main() {
+    
+    // MARK: Program Setup and Entry Point
+    
+    let routeOptimizer = RouteOptimizer()
+    routeOptimizer.run()
+  }
+  
+  init() {
+    
+  }
+  
+  func run() {
+    
+  }
+}
