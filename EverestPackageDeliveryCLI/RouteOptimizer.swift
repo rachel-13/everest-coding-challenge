97d4303d51af64b447d1624e27543b11ef21b8de
Rachel Fong <rach_pohyee13@hotmail.com>
2023-12-24 00:45:03 +0800

    Clean up dirty code

--- EverestPackageDeliveryCLI/RouteOptimizer.swift
+++ EverestPackageDeliveryCLI/RouteOptimizer.swift
@@ -26,44 +26,8 @@ class RouteOptimizer {
     let weightArr = [50, 75, 175, 110, 155, 50]
     let maxWeight = 200
     
-//    let result = findSubsetsLessThanMaxWeight(arr: arr, maxWeight: max)
-//    let result = subsets(arr).filter { currArr in
-//      return currArr.reduce(0, +) <= max
-//    }.sorted { lhs, rhs in
-//      return lhs.count > rhs.count
-//    }
-    
-//    print(result)
-    
     generateDynamicProgrammingTable(maxWeight: maxWeight, weightArr: weightArr)
   }
-  
-  func findSubsetsLessThanMaxWeight(arr: [Int], maxWeight: Int) -> [[Int]] {
-    
-    var subsetsWithinCapacity = [[Int]]()
-    
-    
-    
-    return subsetsWithinCapacity
-  }
-  
-//  func subsets(_ nums: [Int]) -> [[Int]] {
-//    var results: [[Int]] = []
-//    if nums.count == 0 {
-//      return results
-//    }
-//    
-//    results.append([])
-//    
-//    for i in stride(from: 0, to: nums.count, by: 1) {
-//      let count = results.count
-//      for j in stride(from: 0, to: count, by: 1) {
-//        results.append(results[j] + [nums[i]])
-//      }
-//    }
-//    
-//    return results
-//  }
   
   func generateDynamicProgrammingTable(maxWeight:Int, weightArr:[Int]) -> [[Int]] {
     var dpTable = Array(repeating: Array(repeating: -1, count: maxWeight+1), count: weightArr.count+1)
