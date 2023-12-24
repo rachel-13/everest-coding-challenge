b66b4cd771ee48db49c15874825cdaaadd9ba8cc
Rachel Fong <rach_pohyee13@hotmail.com>
2023-12-24 16:00:24 +0800

    Implemented dynamic programming with bookkeeping

--- EverestPackageDeliveryCLI/RouteOptimizer.swift
+++ EverestPackageDeliveryCLI/RouteOptimizer.swift
@@ -23,34 +23,64 @@ class RouteOptimizer {
   }
   
   func run() {
-    let weightArr = [50, 75, 175, 110, 155, 50]
+    let weightArr = [50, 75, 175, 110, 155]
     let maxWeight = 200
     
-    generateDynamicProgrammingTable(maxWeight: maxWeight, weightArr: weightArr)
+    let packageBundle = getOptimalPackageBundle(maxWeight: maxWeight, weightArr: weightArr)
   }
   
-  func generateDynamicProgrammingTable(maxWeight:Int, weightArr:[Int]) -> [[Int]] {
-    var dpTable = Array(repeating: Array(repeating: -1, count: maxWeight+1), count: weightArr.count+1)
+  func getOptimalPackageBundle(maxWeight:Int, weightArr:[Int]) -> [Int] {
+    var countTable = Array(repeating: Array(repeating: -1, count: maxWeight+1), count: weightArr.count+1)
+    var weightTable = Array(repeating: Array(repeating: -1, count: maxWeight+1), count: weightArr.count+1)
+    var selectedItemsTable = Array(repeating: Array(repeating: [Int](), count: maxWeight+1), count: weightArr.count+1)
     
     for item in 0...weightArr.count {
       for weightCap in 0...maxWeight {
 
         if item == 0 || weightCap == 0 {
-          dpTable[item][weightCap] = 0
+          countTable[item][weightCap] = 0
+          weightTable[item][weightCap] = 0
         } else if weightArr[item - 1] <= weightCap {
-          let maxValue = max(dpTable[item - 1][weightCap], weightArr[item - 1] + dpTable[item - 1][weightCap - weightArr[item - 1]])
-          dpTable[item][weightCap] = maxValue
+          
+          let notIncludeCurrentItemWeight = weightTable[item - 1][weightCap]
+          let includeCurrentItemWeight = weightArr[item - 1] + weightTable[item - 1][weightCap - weightArr[item - 1]]
+          
+          let notIncludeCurrentItem = countTable[item - 1][weightCap]
+          let includeCurrentItem = 1 + countTable[item - 1][weightCap - weightArr[item - 1]]
+          
+          if includeCurrentItem > notIncludeCurrentItem {
+            countTable[item][weightCap] = includeCurrentItem
+            weightTable[item][weightCap] = includeCurrentItemWeight
+            
+            if countTable[item - 1][weightCap - weightArr[item - 1]] != 0 {
+              selectedItemsTable[item][weightCap] = selectedItemsTable[item - 1][weightCap - weightArr[item - 1]] + [weightArr[item - 1]]
+            } else {
+              selectedItemsTable[item][weightCap].append(weightArr[item - 1])
+            }
+            
+          } else if notIncludeCurrentItem == includeCurrentItem && includeCurrentItemWeight > notIncludeCurrentItemWeight {
+            countTable[item][weightCap] = includeCurrentItem
+            weightTable[item][weightCap] = includeCurrentItemWeight
+            
+            if countTable[item - 1][weightCap - weightArr[item - 1]] != 0 {
+              selectedItemsTable[item][weightCap] = selectedItemsTable[item - 1][weightCap - weightArr[item - 1]] + [weightArr[item - 1]]
+            } else {
+              selectedItemsTable[item][weightCap].append(weightArr[item - 1])
+            }
+          } else {
+            countTable[item][weightCap] = notIncludeCurrentItem
+            weightTable[item][weightCap] = notIncludeCurrentItemWeight
+            selectedItemsTable[item][weightCap] = selectedItemsTable[item - 1][weightCap]
+          }
+          
         } else {
-          dpTable[item][weightCap] = dpTable[item - 1][weightCap]
+          countTable[item][weightCap] = countTable[item - 1][weightCap]
+          weightTable[item][weightCap] = weightTable[item - 1][weightCap]
+          selectedItemsTable[item][weightCap] = selectedItemsTable[item - 1][weightCap]
         }
-    
       }
     }
     
-//    dpTable.forEach { currElement in
-//      print(currElement)
-//    }
-    
-    return dpTable
+    return selectedItemsTable[weightArr.count][maxWeight]
   }
 }
