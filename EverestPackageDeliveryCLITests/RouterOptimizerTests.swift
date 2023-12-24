7ca83034ecc36c3e69035b043e805c19a36f52c0
Rachel Fong <rach_pohyee13@hotmail.com>
2023-12-24 16:06:30 +0800

    Simple test case for route optimizer

--- EverestPackageDeliveryCLITests/RouterOptimizerTests.swift
+++ EverestPackageDeliveryCLITests/RouterOptimizerTests.swift
@@ -8,9 +8,23 @@
 import XCTest
 
 final class RouterOptimizerTests: XCTestCase {
+  
+  var sut: RouteOptimizer!
 
   override func setUp() {
-    
+    sut = RouteOptimizer()
+  }
+  
+  func testGetHeaviestBundle() {
+    let bundle = sut.getOptimalPackageBundle(maxWeight: 200,
+                                             weightArr: [50, 75, 175, 110, 155])
+    XCTAssertEqual(bundle, [1, 3])
+  }
+  
+  func testGetNonHeaviestWithMaxPackages() {
+    let bundle = sut.getOptimalPackageBundle(maxWeight: 200,
+                                             weightArr: [50, 75, 175, 110, 155, 50])
+    XCTAssertEqual(bundle, [0, 1, 5])
   }
 
 }
